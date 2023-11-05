cc0d039a587cebbf2ab8c21e06d229c4f5578b27
n.podgurskiy <mortarionggl@gmail.com>
2023-11-05 12:41:51 +0200

    Initial code

---
+++ src/telran/tools/calc.java
@@ -0,0 +1,24 @@
+package telran.tools;
+
+public class calc {
+	public static double sum(double op1, double op2) 
+	{
+		return op1 + op2;
+	}
+	public static double subctract(double op1, double op2) 
+	{
+		return op1 - op2;
+	}
+	public static double multipied(double op1, double op2) 
+	{
+		return op1 * op2;
+	}
+	public static double divide(double op1, double op2) 
+	{
+		return op1 / op2;
+	}
+}
+
+//чтобы создать репозиторию для гит - проект - team - share - выбрать папку
+//посмотреть репозиторий - team - show in repositories view
+//инструменты гит - window - show view - other - git staging
